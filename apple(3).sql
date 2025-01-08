12d74fc5a73a879657a5cf1a3290813f6ddf8a79
Harsha Sugali <sharshavardhannaik@gmail.com>
2025-01-08 19:52:12 +0530

    Delete apple(3).sql

--- apple(3).sql
+++
@@ -1,9 +0,0 @@
-/* 3)How many sales occurred in December 2023?*/
-
-SELECT 
-    COUNT(sale_id) AS total_sales
-FROM
-    sales
-WHERE
-    YEAR(sale_date) = '2023'
-        AND MONTH(sale_date) = '12';
